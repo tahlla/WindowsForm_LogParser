e34a0029b4a566517e0d06a3324e53a45859ae3d
Tahlla Taylor <tahlla.rsa@gmail.com>
2020-11-11 22:12:44 -0600

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsForms_LogParse2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(48, 36);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(418, 20);
+            this.maskedTextBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 85);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(56, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(259, 92);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Location = new System.Drawing.Point(472, 12);
+            this.textBox1.MinimumSize = new System.Drawing.Size(400, 400);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(400, 400);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿//bring these in to use different methods and functions
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+//name of program
+namespace WindowsForms_LogParse2
+{
+ //will link to the Form (GUI)
+    public partial class Form1 : Form
+    {
+        //declaring a variable for later use, and it needed to be declared outside of the button
+        string filename;
+
+        //Tells the form to initialize - shows button/text file box on GUI
+            public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Creates an object to store parsed data and its count
+        //public class declares a class that has variables and functions
+        //a class can be used as an object which is the basis of object oriented programing
+        public class DataString
+        {
+            public string dataname { get; set; }
+            public int datacount { get; set; }
+        }
+
+        //Declare and initialize a list of the DataString object 
+        List<DataString> parseddata = new List<DataString>();
+
+        //This method will execute when button one is clicked
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if a new file is selected then this clears the text box 
+            textBox1.Clear();
+            //this will clear the list of DataString objects is cleared
+            parseddata.Clear();
+
+           
+            //creates an object to open the file explorer
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            //If the file explorer opens and everything is okay then the file selected will be uploaded as file name
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string strfilename = openFileDialog1.FileName;
+                maskedTextBox1.Text = strfilename;
+                filename = strfilename;
+            }
+        }
+
+
+
+         
+        
+        //This is button2 and will parse data that will be shown in textbox2
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            // Read lines from source file
+            string[] arr = File.ReadAllLines(filename);
+            
+            // x = 0 declare and initializes x for the loop set to 0
+            //for each is a loop that will go over and over 
+            int x = 0;
+            foreach (string value in arr)
+            {
+                
+             //Will parse the data read from the file into an array 
+             //, \n are delimiters that can split the information 
+             //a , or newline will be a chunck of data
+            
+                string[] tempdataholder = arr[x].Split(new Char[] { ',', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                x++;
+
+                //This for loop will check the list for the string from tempdataholder (array of strings) 
+                //If the string is found it will increase the count for that string 
+                //If the string is not found it will add the string to the list as a DataString object and make the count 1
+                foreach (string tempstring in tempdataholder)
+                {
+                    bool found = false;
+                    foreach (DataString y in parseddata)
+                    {
+                        if (tempstring == y.dataname)
+                        {
+                            found = true;
+                            y.datacount += 1;
+                        }
+                    }
+                    if (found == false)
+                    {
+                        DataString temp1 = new DataString();
+                        temp1.dataname = tempstring;
+                        temp1.datacount = 1;
+                        parseddata.Add(temp1);
+                    }
+                }
+
+
+
+                
+            }
+            //This loop will print each DataString object and its count
+            //is a random variable name to represent the object that will be used to 
+            foreach (DataString z in parseddata)
+            {
+                //Show Item: before dataname 
+                textBox1.AppendText("Item: ");
+                textBox1.AppendText(z.dataname);
+                //Show | Count: before the datacount
+                textBox1.AppendText(" | Count: ");
+                int num = z.datacount;
+                textBox1.AppendText(num.ToString());
+                //\r is a carriage return that will move all the way to the left on the next line
+                //\n will tell it to also go to a new line at the next line when printing it
+                textBox1.AppendText("\r\n");
+
+            }
+
+        }
+    }
+}
+
+
